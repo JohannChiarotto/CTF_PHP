19e9a66cc9e02a697a9162aa1af1aa618a3da9d3
cjohann <chiarottojohann2@gmail.com>
2026-02-19 20:25:07 +0100

    trie main pré push final

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'db.php';
-echo "<h1>Bienvenue sur la plateforme CTF !</h1>";
-if ($mysqli) {
-    echo "<p style='color:green'>✅ Connexion à la base de données réussie.</p>";
-}
-?>
